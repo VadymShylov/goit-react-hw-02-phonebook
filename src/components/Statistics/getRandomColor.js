9aaf73a0a8a2f35bd33df1ae66b573b064ac527d
VadymShylov <shylovvadym@gmail.com>
2022-09-12 20:11:08 +0100

    created

--- src/components/Statistics/getRandomColor.js
+++
@@ -1,8 +0,0 @@
-const getRandomColor = () => {
-  const r = Math.round(Math.random() * 256);
-  const g = Math.round(Math.random() * 256);
-  const b = Math.round(Math.random() * 256);
-  return `rgb(${r}, ${g}, ${b})`;
-};
-
-export default getRandomColor;
